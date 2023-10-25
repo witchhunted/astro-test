6e45152087c204ab0df27af0414bd5c17b5dbb78
panatte <panat.te@bumail.net>
2023-10-25 16:59:02 +0700

    Initial commit

---
+++ src/js/vanta.js
@@ -0,0 +1,13 @@
+    VANTA.BIRDS({
+    el: "#vanta",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    birdSize: 0.50,
+    speedLimit: 1.00,
+    separation: 100.00
+    })
